97ab99d85555793d74e40c50ac10ce894caeb689
makar0nkin <ilya.makarenko2004@gmail.com>
2024-10-20 21:29:54 +0300

    fix: MSE L2 Loss in hw05

--- homeworks/hw04-knn-linreg/test.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import load_wine
-import pandas as pd
-
-data = load_wine()
-X = pd.DataFrame(data['data'], columns = data['feature_names'])
-y = data['target']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
